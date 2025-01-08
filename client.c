ee40e92fe4c64ce7fb8f7ed71ea38e9d7ad941d9
Will Reed <willr343@mit.edu>
2025-01-08 15:18:02 -0500

    created client file

---
+++ client.c
@@ -0,0 +1,18 @@
+#include "tic_tac_toe.h"
+#include <arpa/inet.h>
+#include <errno.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+#define PORT "6060"
+#define BACKLOG 10
+
+int main() {}
